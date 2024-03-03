449302d0bfa5f3536eb2ca3d416a9dbf96d09d15
Dipendra Nath <dipen.2052@gmail.com>
2024-03-03 06:53:34 -0500

    inital project setup done

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User CRUD API Documentation</title>
+</head>
+<body>
+    <h1>Simple E-Commerce CRUD API Documentation</h1>
+    <p>This API allows you to perform CRUD operations (Create, Read, Update, Delete) on user data.</p>
+
+  
+</body>
+</html>
