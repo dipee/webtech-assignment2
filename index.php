d21d6e63c5234638e12632e664c6aaa06cd59725
Dipendra Nath <dipen.2052@gmail.com>
2024-03-03 07:21:18 -0500

    added syles page

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User CRUD API Documentation</title>
+    <!-- import css file -->
+    <link rel="stylesheet" href="css/style.css">
+    
 </head>
 <body>
     <h1>Simple E-Commerce CRUD API Documentation</h1>
-    <p>This API allows you to perform CRUD operations (Create, Read, Update, Delete) on user data.</p>
+    <p>This API allows you to perform CRUD operations (Create, Read, Update, Delete) data.</p>
 
   
 </body>
